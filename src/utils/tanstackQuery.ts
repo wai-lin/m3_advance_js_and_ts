9309adfec6aeed38318b0518e11115a8e17b08e7
wai-lin <dev.wailin@gmail.com>
2025-10-22 16:45:11 +0700

    chore: reset main branch to empty project

--- src/utils/tanstackQuery.ts
+++
@@ -1,9 +0,0 @@
-import { QueryClient, useMutation, useQuery } from "@tanstack/solid-query";
-
-export { useMutation, useQuery };
-
-export const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: { retry: 3 },
-	},
-});
