78911c36bc5e79397a6898172396bf680f9f6554
wai-lin <dev.wailin@gmail.com>
2025-10-30 21:43:49 +0700

    feat(web): date format accept undefined

--- apps/web/src/utils/date-format.ts
+++ apps/web/src/utils/date-format.ts
@@ -1,5 +1,6 @@
 import dayjs from "dayjs";
 
-export function formatDate(d: Date | string): string {
+export function formatDate(d?: Date | string): string {
+	if (!d) return "";
 	return dayjs(new Date(d)).format("DD MMM, YYYY");
 }
