f44de9c987db1085bd2a80ed59eab657846ca723
wai-lin <dev.wailin@gmail.com>
2025-11-02 18:52:53 +0700

    feat(web): blog editor form accept default values

--- apps/web/src/BlogEditorForm/BlogEditorForm.tsx
+++ apps/web/src/BlogEditorForm/BlogEditorForm.tsx
@@ -4,8 +4,15 @@ import type { FormOnSubmit } from "../Components";
 import { Form, FormField } from "../Components";
 import { TextEditor } from "../TextEditor";
 
+interface FormValues {
+	title: string;
+	slug: string;
+	content: string;
+}
+
 interface Props {
 	id?: string;
+	defaultValues?: FormValues;
 	class?: ClassValue[] | string;
 	onSubmit?: FormOnSubmit;
 }
@@ -13,15 +20,31 @@ export const BlogEditorForm: ParentComponent<Props> = (props) => {
 	return (
 		<Form id={props.id} onSubmit={props.onSubmit} class={props.class}>
 			<FormField for="title" label="Title:" required>
-				<input required id="title" name="title" type="text" class="input w-full" />
+				<input
+					required
+					id="title"
+					name="title"
+					type="text"
+					value={props.defaultValues?.title}
+					class="input w-full"
+				/>
 			</FormField>
 
 			<FormField for="slug" label="Slug:">
-				<input id="slug" name="slug" type="text" class="input w-full" />
+				<input
+					id="slug"
+					name="slug"
+					type="text"
+					value={props.defaultValues?.slug}
+					class="input w-full"
+				/>
 			</FormField>
 
-			<FormField for="content" label="Content:" required>
-				<TextEditor name="content" />
+			<FormField label="Content:" required>
+				<TextEditor
+					name="content"
+					defaultValue={props.defaultValues?.content}
+				/>
 			</FormField>
 
 			{props.children}
