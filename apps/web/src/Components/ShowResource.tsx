ff15b0685af5d8474757fa1c2551a8854fe9cbbd
wai-lin <dev.wailin@gmail.com>
2025-11-02 17:49:49 +0700

    feat(web): show resource component align with tanstack query

--- apps/web/src/Components/ShowResource.tsx
+++ apps/web/src/Components/ShowResource.tsx
@@ -1,38 +1,39 @@
-import type { Component, ParentComponent, Resource } from "solid-js";
+import type { UseQueryResult } from "@tanstack/solid-query";
+import type { Component, ParentComponent } from "solid-js";
 import { Match, Switch } from "solid-js";
 import { Heading } from "./Heading";
 
 interface Props {
 	loading?: Component;
 	error?: Component;
-	resource: Resource<unknown>;
+	resource: UseQueryResult<unknown>;
 }
 
 export const ShowResource: ParentComponent<Props> = (props) => {
 	return (
 		<Switch>
-			<Match when={props.resource.loading && props.loading}>
+			<Match when={props.resource.isPending && props.loading}>
 				{props.loading}
 			</Match>
 
-			<Match when={props.resource.loading && !props.loading}>
+			<Match when={props.resource.isPending && !props.loading}>
 				<Heading as="h4">Loading...</Heading>
 			</Match>
 
-			<Match when={props.resource.error && props.error}>
+			<Match when={props.resource.isError && props.error}>
 				{props.error}
 			</Match>
 
-			<Match when={props.resource.error && !props.error}>
+			<Match when={props.resource.isError && !props.error}>
 				<div class="card bg-base-100 shadow-sm">
 					<div class="card-body">
 						<h3 class="card-title">Error</h3>
-						<div>{props.resource.error}</div>
+						<div>{props.resource.error?.message}</div>
 					</div>
 				</div>
 			</Match>
 
-			<Match when={props.resource()}>
+			<Match when={props.resource.data}>
 				{props.children}
 			</Match>
 		</Switch>
