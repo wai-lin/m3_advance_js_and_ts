07b818d2d7e660cee1063d40f2b8c2bf61a84ef0
wai-lin <dev.wailin@gmail.com>
2025-11-02 17:55:48 +0700

    feat(web): update routes

---
+++ apps/web/src/routes.tsx
@@ -0,0 +1,14 @@
+import type { Component } from "solid-js";
+import { Route, Router } from "@solidjs/router";
+import { lazy } from "solid-js";
+import BlogList from "./BlogList";
+
+export const Routes: Component = () => {
+	return (
+		<Router>
+			<Route path="/" component={BlogList} />
+			<Route path="/create" component={lazy(() => import("./BlogCreateForm"))} />
+			<Route path="/:blog" component={lazy(() => import("./BlogPreview"))} />
+		</Router>
+	);
+};
